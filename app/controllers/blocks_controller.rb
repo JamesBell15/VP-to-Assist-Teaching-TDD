5a2dad928591b61f3fde913f7bf79dc300f63751
JamesBell15 <73955873+JamesBell15@users.noreply.github.com>
2023-03-01 10:51:24 +0000

    Refactor block controller

--- app/controllers/blocks_controller.rb
+++ app/controllers/blocks_controller.rb
@@ -2,16 +2,28 @@ class BlocksController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    file, code = params[:block].split("__")
+    path, code = params[:block].split("__")
 
-    File.open("spec/" + file, "w") { |f|
-      f.write code
-    }
+    spec_path = "spec/#{path}_spec.rb"
+
+    if valid_path?(spec_path)
+      write_spec(spec_path)
+    end
   end
 
   def block_params
     params.require(:block)
   end
+
+  private
+
+  def valid_path?(path)
+    File.directory?(path)
+  end
+
+  def write_spec(path)
+    File.open(spec_path, "w") { |f|
+      f.write code
+    }
+  end
 end
-
-
