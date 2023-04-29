d6c9dbda70b84862de66b7272924f966ae0bb5df
JamesBell15 <73955873+JamesBell15@users.noreply.github.com>
2023-04-29 16:51:09 +0100

    Update view to reflect controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  root "blogs#index"
+  root "blocks#index"
 
   resources :blogs, only: [:index, :show]
-  resources :blocks, only: :create
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :blocks, only: [:index, :create]
 end
