ec965ecddeddcf72feb82f7007c2a5d7564af7b0
JamesBell15 <73955873+JamesBell15@users.noreply.github.com>
2023-05-03 12:57:03 +0100

    Clean up toolbox

--- app/assets/javascript/ruby_toolbox.js
+++ app/assets/javascript/ruby_toolbox.js
@@ -7,113 +7,80 @@
 
 
 function handlePlay(event) {
-  loadWorkspace(event.target)
-
   let code = rubyGenerator.workspaceToCode(workspace)
 
   try {
     document.getElementById("block").value = code
-    // showBanner()
+    showBanner()
 
   } catch (error) {
     console.log(error)
   }
 }
 
-function save(button) {
-  // button.blocklySave = Blockly.serialization.workspaces.save(Blockly.common.getMainWorkspace())
-}
-
-function loadWorkspace(button) {
-  const workspace = Blockly.common.getMainWorkspace()
-  if (button.blocklySave) {
-    Blockly.serialization.workspaces.load(button.blocklySave, workspace)
-  }
-}
-
-function handleSave() {
-  document.body.setAttribute('mode', 'edit')
-  save(currentButton)
-}
-
-function enableEditMode() {
-  document.body.setAttribute('mode', 'edit')
-  let buttons = document.getElementsByTagName("button")
-  Array.from(buttons).map(btn => {
-    btn.removeEventListener('click', handlePlay)
-    btn.addEventListener('click', enableBlocklyMode)
-  })
-}
-
-function enableMakerMode() {
-  document.body.setAttribute('mode', 'maker')
-  let buttons = document.getElementsByTagName("button")
-  Array.from(buttons).map(btn => {
-    btn.addEventListener('click', handlePlay)
-    btn.removeEventListener('click', enableBlocklyMode)
-  })
-}
-
-function enableBlocklyMode(e) {
-  document.body.setAttribute('mode', 'blockly')
-  currentButton = e.target
-  loadWorkspace(currentButton)
-}
 
 let currentButton
 
-document.querySelector('#edit').addEventListener('click', enableEditMode)
-document.querySelector('#done').addEventListener('click', enableMakerMode)
-document.querySelector('#save').addEventListener('click', handleSave)
-
-enableMakerMode()
+document.querySelector('#done').addEventListener('click', handlePlay)
 
 const toolbox = {
-  "kind": "flyoutToolbox",
+  "kind": "categoryToolbox",
   "contents": [
     {
-      "kind": "block",
-      "type": "text"
+      "kind": "category",
+      "name": "Testing",
+      "contents": [
+        {
+          "kind": "block",
+          "type": "path_to"
+        },
+        {
+          "kind": "block",
+          "type": "describe"
+        },
+        {
+          "kind": "block",
+          "type": "context"
+        },
+        {
+          "kind": "block",
+          "type": "let"
+        },
+        {
+          "kind": "block",
+          "type": "statement_block"
+        },
+        {
+          "kind": "block",
+          "type": "it"
+        },
+        {
+          "kind": "block",
+          "type": "expect"
+        },
+        {
+          "kind": "block",
+          "type": "eq"
+        }
+      ]
     },
     {
-      "kind": "block",
-      "type": "logic_null"
-    },
-    {
-      "kind": "block",
-      "type": "p"
-    },
-    {
-      "kind": "block",
-      "type": "describe"
-    },
-    {
-      "kind": "block",
-      "type": "context"
-    },
-    {
-      "kind": "block",
-      "type": "it"
-    },
-    {
-      "kind": "block",
-      "type": "expect"
-    },
-    {
-      "kind": "block",
-      "type": "eq"
-    },
-    {
-      "kind": "block",
-      "type": "path_to"
-    },
-    {
-      "kind": "block",
-      "type": "let"
-    },
-    {
-      "kind": "block",
-      "type": "statement_block"
+      "kind": "category",
+      "name": "Misc",
+      "contents": [
+        {
+          "kind": "block",
+          "type": "p"
+        },
+        {
+          "kind": "block",
+          "type": "text"
+        },
+        {
+          "kind": "block",
+          "type": "logic_null"
+        }
+      ]
     }
   ]
 }
@@ -143,6 +110,8 @@ const updateCode = (event) => {
   document.getElementById('textarea').innerHTML = code.replace('__', '')
 }
 
+// to keep toolbox open after block selection
+workspace.toolbox_.flyout_.autoClose = false
 workspace.addChangeListener(updateCode)
 
 
